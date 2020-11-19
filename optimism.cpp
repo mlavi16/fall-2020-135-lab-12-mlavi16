de9890e06882e39ae5e21ffa34e378b412473514
mlavi16 <Maya.lavi11@gmail.com>
2020-11-18 19:51:09 -0500

    adding comment for func's purpose

--- optimism.cpp
+++ optimism.cpp
@@ -2,6 +2,7 @@
 #include "optimism.h"
 
 std::vector<int> goodVibes(const std::vector<int> & v) {
+    // returns a vector with only the positive integers in the same order as the given vector.
     std::vector<int> new_v;
     for (int i = 0; i < v.size(); i++) {
         if (v[i] >= 0){
