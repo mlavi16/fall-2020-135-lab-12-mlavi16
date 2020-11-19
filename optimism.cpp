bf22368b8b0ad2d8f451fb7c5a164bd61d442fbe
mlavi16 <Maya.lavi11@gmail.com>
2020-11-18 19:23:14 -0500

    fixing for loop condition

--- optimism.cpp
+++ optimism.cpp
@@ -3,7 +3,7 @@
 
 std::vector<int> goodVibes(const std::vector<int> & v) {
     std::vector<int> new_v;
-    for (int i = 0; i < (v.size() - 1); i++) {
+    for (int i = 0; i < v.size(); i++) {
         if (v[i] >= 0){
             new_v.push_back(v[i]);
         }
